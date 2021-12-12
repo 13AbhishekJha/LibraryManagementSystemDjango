17dad7c569890dc626741685b14674167c162acd
Abhishek Jha <Abhishek@Jha.com>
2021-12-12 14:01:01 +0530

    added student-admin an configure settings file

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path,include
+from django.conf import settings 
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+admin.autodiscover()
+
+admin.site.index_title='Library Management'
+admin.site.site_header='Admin Dashboard' 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('student/',include('student.urls')),
 ]
+    # path('',include('library.urls')),
+# ]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
